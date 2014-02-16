d5da50de57d7b70577f60af9bdbfb96f52fb32b5
Hector Zarate <hecktorzr@gmail.com>
2014-02-16 19:22:04 +0100

    Added colors and correct matrix drawing

--- ConwayExample/ConwayExample/CWYViewController.m
+++ ConwayExample/ConwayExample/CWYViewController.m
@@ -6,8 +6,12 @@
 //  Copyright (c) 2014 Hector Zarate. All rights reserved.
 //
 
+#import <CoreGraphics/CoreGraphics.h>
 #import "CWYViewController.h"
 #import "CWYCelullarMatrix.h"
+
+const NSUInteger CWYGridFactor = 8;
+
 @interface CWYViewController ()
 
 @property (nonatomic, strong) IBOutlet UIView *cellularView;
@@ -21,9 +25,11 @@
 
 - (void)viewDidLoad
 {
-    self.cellularMatrix = [[CWYCelullarMatrix alloc] initWithWidth:self.cellularView.bounds.size.width+1
-                                                            height:self.cellularView.bounds.size.height+1];
+    self.cellularMatrix = [[CWYCelullarMatrix alloc] initWithWidth:(self.cellularView.bounds.size.width / CWYGridFactor) + 1
+                                                            height:(self.cellularView.bounds.size.height / CWYGridFactor) + 1];
     
+    
+    [self setupMatrix];
     [self drawMatrix];
     
     [super viewDidLoad];
@@ -32,43 +38,64 @@
 
 -(void)nextGeneration:(id)sender
 {
-    NSLog(@"%@", self.cellularMatrix);
-    
     [self.cellularView.subviews makeObjectsPerformSelector:@selector(removeFromSuperview)];
     
     [self.cellularMatrix nextGeneration];
-
-    NSLog(@"%@", self.cellularMatrix);
     
     [self drawMatrix];
 }
 
-- (void)drawMatrix
+- (void)setupMatrix
 {
     for (int matrixIndex = 0;
-         matrixIndex < self.cellularMatrix.size.width * self.cellularMatrix.size.height;
+         matrixIndex < self.cellularMatrix.length;
          ++matrixIndex)
     {
-        if (self.cellularMatrix.currentMatrix[matrixIndex])
+        CALayer *layer = [[CALayer alloc] init];
+        
+        CGPoint point = [self.cellularMatrix pointFromVectorIndex:matrixIndex];
+        layer.borderWidth = 0.0;
+        layer.borderColor = [UIColor grayColor].CGColor;
+        layer.frame = CGRectMake(point.x * CWYGridFactor,
+                                 point.y * CWYGridFactor,
+                                 CWYGridFactor,
+                                 CWYGridFactor);
+        
+        layer.backgroundColor = [UIColor purpleColor].CGColor;
+        layer.opacity = 0.0;
+        layer.cornerRadius = CWYGridFactor / 2.0;
+        [self.cellularView.layer addSublayer:layer];
+
+    }
+    
+}
+
+- (void)drawMatrix
+{
+    [CATransaction begin];
+    [CATransaction setAnimationDuration:0.5];
+    [CATransaction setAnimationTimingFunction:[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut]];
+    for (int matrixIndex = 0;
+         matrixIndex < self.cellularMatrix.length;
+         ++matrixIndex)
+    {
+        
+        CALayer *layer = self.cellularView.layer.sublayers[matrixIndex];
+        
+        if (self.cellularMatrix.currentMatrix[matrixIndex] == YES)
         {
-            CGPoint point = [self.cellularMatrix pointFromVectorIndex:matrixIndex];
-            
-            UIView *viewTest = [[UIView alloc] initWithFrame:CGRectMake(point.x, point.y, 1, 1)];
-            viewTest.backgroundColor = [UIColor redColor];
-            
-            [self.cellularView addSubview:viewTest];
-        }/*
+            layer.opacity = 1.0;
+            layer.bounds = CGRectMake(0.0, 0.0, CWYGridFactor, CWYGridFactor);
+
+        }
         else
         {
-            CGPoint point = [self.cellularMatrix pointFromVectorIndex:matrixIndex];
-            
-            UIView *viewTest = [[UIView alloc] initWithFrame:CGRectMake(point.x, point.y, 1, 1)];
-            viewTest.backgroundColor = [UIColor blackColor];
-            
-            [self.cellularView addSubview:viewTest];
-            
-        }*/
+            layer.opacity = 0.0;
+            layer.bounds = CGRectMake(0.0, 0.0, 0.0, 0.0);
+        }
+
     }
+    [CATransaction commit];
 }
 
 - (void)didReceiveMemoryWarning
@@ -85,15 +112,19 @@
     
     CGPoint touchedPoint =[touch locationInView:self.view];
     
-    NSUInteger index = [self.cellularMatrix vectorIndexFromPointX:touchedPoint.x pointY:touchedPoint.y];
+    NSUInteger index = [self.cellularMatrix vectorIndexFromPointX:touchedPoint.x/CWYGridFactor
+
+                                                           pointY:touchedPoint.y/CWYGridFactor];
     
     self.cellularMatrix.currentMatrix[index] = YES;
     
-    
     NSLog(@"Point: %@", NSStringFromCGPoint(touchedPoint));
-    
-    [self drawMatrix];
+
 }
 
+-(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [self drawMatrix];
+}
 
 @end
